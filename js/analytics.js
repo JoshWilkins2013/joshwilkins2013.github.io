8e676b4adb9cb648d8658f8824ed9cfcfa600ca9
joshwilkins2013 <joshwilkins2013@gmail.com>
2018-04-14 11:23:29 -0400

    Updated analytics page

--- js/analytics.js
+++ js/analytics.js
@@ -1,10 +1,36 @@
 var albums = {
+		"MakeModel": [
+			"<img src=\"img/Analytics/Cars/MakeModelComparison.png\"><p>Model Comparison</p>",
+			"<img src=\"img/Analytics/Cars/Results.png\"><p>Best New Car Models</p>"
+		],
+		"PriceBot": [
+			"<img src=\"img/Analytics/PriceBot/Cars_Output.svg\"><p>Merged Honda Civic Results</p>",
+			"<img src=\"img/Analytics/PriceBot/Apt_Output.svg\"><p>Merged Apt Results</p>"
+		],
 		"House": [
 			"<img src=\"img/Analytics/House/Typical.png\"><p>Typical Scenario</p>",
 			"<img src=\"img/Analytics/House/Loan_Options.png\"><p>Loan Options</p>",
 			"<img src=\"img/Analytics/House/Rental_Income.png\"><p>Rental Income</p>",
 			"<img src=\"img/Analytics/House/IRA_Contributions.png\"><p>IRA Investments</p>"
 		]
+}
+
+var PriceBotSlideIndex= 1;
+PriceBotSlides(PriceBotSlideIndex);
+function PriceBotSlides(n) {
+  var i;
+  if (n > albums["PriceBot"].length) {PriceBotSlideIndex = 1}
+  if (n < 1) {PriceBotSlideIndex = albums["PriceBot"].length}
+  document.getElementById("PriceBotSlider").innerHTML = albums["PriceBot"][PriceBotSlideIndex-1];
+}
+
+var MakeModelSlideIndex= 1;
+MakeModelSlides(MakeModelSlideIndex);
+function MakeModelSlides(n) {
+  var i;
+  if (n > albums["MakeModel"].length) {MakeModelSlideIndex = 1}
+  if (n < 1) {MakeModelSlideIndex = albums["MakeModel"].length}
+  document.getElementById("MakeModelSlider").innerHTML = albums["MakeModel"][MakeModelSlideIndex-1];
 }
 
 var HouseSlideIndex= 1;
