20680b28064fc4eb81bccb6fad9a29859c8d5bc7
Joshua Wilkins <JoshWilkins2013@gmail.com>
2025-02-08 15:54:00 -0500

    Added Maine, moved stuff out of js by making a block class

---
+++ js/block.js
@@ -0,0 +1,21 @@
+var SlideIndex= 1;
+
+function Slides(n, image_paths, name) {
+  var image_array = image_paths.split(",");
+  if (n > image_array.length - 1) {SlideIndex = 1}
+  if (n < 1) {SlideIndex = image_array.length - 1}
+  current_image_path = image_array[SlideIndex-1];
+  current_image_name = current_image_path.split("/").at(-1).split(".")[0].replaceAll("_", " ");
+  document.getElementById(name + "_Image").src = current_image_path;
+  document.getElementById(name + "_Caption").innerHTML = current_image_name;
+}
+
+function load_html(project_name, html_path) {
+    var description = "<iframe src=" + html_path + " style=\"width: 100%;border: none;\"></iframe>"
+    document.getElementById(project_name + "_Content").innerHTML = description;
+}
+
+function load_pdf(project_name, pdf_path) {
+    var description = "<iframe src=" + pdf_path + "#toolbar=0&view=FitH\" style=\"width: 100%;border: none;\"></iframe>"
+    document.getElementById(project_name + "_Content").innerHTML = description;
+}
